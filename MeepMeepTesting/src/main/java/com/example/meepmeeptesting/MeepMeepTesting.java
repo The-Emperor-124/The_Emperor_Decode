53e7770006859887272dc37767fb1bd6faf6d7f1
Daria-1303 <daria.ciobanu@student.upt.ro>
2025-11-27 19:54:56 +0200

    feat: add MeepMeep testing module with basic trajectory simulation. _. deprecated version but it works for now

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,39 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Rotation2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 4420.985173528618)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-39.9, -70.1, 89.5)) //17.8 70.1
+                .strafeToConstantHeading(new Vector2d(-59,-59))
+                .build());
+
+        Image img=null;
+        try{img= ImageIO.read(new File("C:/Users/daria/Documents/GitHub/Robotica/The_Emperor_Decode/MeepMeepTesting/image.png"));}
+        catch(IOException e){}
+
+        meepMeep.setBackground(img)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
