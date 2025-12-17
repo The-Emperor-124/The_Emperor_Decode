7f2c7d45ae8a5d4267b83c09cbb81b886739ccd2
Zvipy <channelhere2020@gmail.com>
2025-12-17 16:13:45 +0200

    intake poate fi oprit/pornit cu o singura apasare

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/Mecanum2026.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/Mecanum2026.java
@@ -22,6 +22,7 @@ public class Mecanum2026 extends LinearOpMode {
 
         while (opModeIsActive()) {
             double power,powerIntake;
+            // boolean aNow,aLast = false,intakeOn = false;
             // turbo slow and normal modes
             if (gamepad1.left_trigger > 0.0) {
                 power = 0.2;
@@ -33,6 +34,15 @@ public class Mecanum2026 extends LinearOpMode {
            if(gamepad2.a) {
              powerIntake=1.0;}
            else {powerIntake=0.0;}
+           /* aNow = gamepad1.a;
+           if(aNow && !aLast)
+               intakeOn = !intakeOn;
+
+           aLast = aNow;
+           if(intakeOn)
+               powerIntake = 1.0;
+           else
+               powerIntake = 0.0; */
 
             // Using the new modular hardware class to drive
             robot.driveMitza(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, power);
