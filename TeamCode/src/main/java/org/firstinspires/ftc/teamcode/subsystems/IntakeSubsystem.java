b091bc5f88c9cd153746da9fbfd79fabf1f3c113
Daria-1303 <daria.ciobanu@student.upt.ro>
2026-01-05 21:14:44 +0200

    feat: Implement initial robot hardware subsystems for Mecanum drive, intake, and outtake, along with basic teleop control.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class IntakeSubsystem {
+    public DcMotor motorIntake = null;
+
+    public void init(HardwareMap hardwareMap) {
+        motorIntake = hardwareMap.get(DcMotor.class, "motorIntake");
+        motorIntake.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+    public void setPower(double power) {
+        motorIntake.setPower(power);
+    }
+}
